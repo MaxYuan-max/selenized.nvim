489b765a96f1e96894e28ede590838902983a179
MaxYuan-max <62113305+MaxYuan-max@users.noreply.github.com>
2024-01-27 18:30:37 +0800

    Create selenized.lua

---
+++ lua/lualine/themes/selenized.lua
@@ -0,0 +1,24 @@
+return {
+    normal = {
+        a = { fg = '#103c48', bg = '#4695f7', gui = 'bold' },
+        b = { fg = '#cad8d9', bg = '#103c48' },
+        c = { fg = '#cad8d9', bg = '#103c48' },
+    },
+    insert = {
+        a = { fg = '#103c48', bg = '#75b938', gui = 'bold' },
+    },
+    visual = {
+        a = { fg = '#103c48', bg = '#ed8649', gui = 'bold' },
+    },
+    replace = {
+        a = { fg = '#103c48', bg = '#fa5750', gui = 'bold' },
+    },
+    inactive = {
+        a = { fg = '#325b66', bg = '#103c48', gui = 'bold' },
+        b = { fg = '#325b66', bg = '#103c48' },
+        c = { fg = '#325b66', bg = '#103c48' },
+    },
+    command = {
+        a = { bg = '#dbb32d', fg = '#103c48', gui = 'bold' },
+    },
+}
