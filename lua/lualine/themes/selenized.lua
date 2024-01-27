3b688640fea77a5dc1f3896fa42f4237f131ac0f
MaxYuan-max <62113305+MaxYuan-max@users.noreply.github.com>
2024-01-27 18:46:15 +0800

    Update selenized.lua

--- lua/lualine/themes/selenized.lua
+++ lua/lualine/themes/selenized.lua
@@ -2,7 +2,7 @@ return {
     normal = {
         a = { fg = '#103c48', bg = '#4695f7', gui = 'bold' },
         b = { fg = '#cad8d9', bg = '#103c48' },
-        c = { fg = '#cad8d9', bg = '#103c48' },
+        c = { fg = '#cad8d9', bg = '#174956' },
     },
     insert = {
         a = { fg = '#103c48', bg = '#75b938', gui = 'bold' },
@@ -14,9 +14,9 @@ return {
         a = { fg = '#103c48', bg = '#fa5750', gui = 'bold' },
     },
     inactive = {
-        a = { fg = '#72898f', bg = '#103c48', gui = 'bold' },
-        b = { fg = '#72898f', bg = '#103c48' },
-        c = { fg = '#72898f', bg = '#103c48' },
+        a = { fg = '#72898f', bg = '#174956', gui = 'bold' },
+        b = { fg = '#72898f', bg = '#174956' },
+        c = { fg = '#72898f', bg = '#174956' },
     },
     command = {
         a = { bg = '#dbb32d', fg = '#103c48', gui = 'bold' },
